22d423fff23fc7079a2ce2eed26f76bf347a9110
nafsan <iamnafsan@gmail.com>
2019-07-20 05:29:27 +0600

    raat 5 ta

---
+++ app/src/main/java/com/csedu/bracathon/DengueActivity.java
@@ -0,0 +1,15 @@
+package com.csedu.bracathon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DengueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dengue);
+        setTitle("ডেঙ্গু রোগের লক্ষণ");
+
+    }
+}
--- app/src/main/java/com/csedu/bracathon/NotificationActivity.java
+++ app/src/main/java/com/csedu/bracathon/NotificationActivity.java
@@ -1,14 +1,25 @@
 package com.csedu.bracathon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NotificationActivity extends AppCompatActivity {
-
+    Button dengue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
         setTitle("Notification");
+
+        dengue = findViewById(R.id.dengue);
+        dengue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NotificationActivity.this, DengueActivity.class);
+            }
+        });
     }
 }
